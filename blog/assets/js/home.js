7dd0515a3b764c5839b1d7a2bd0c8aa81e670b6b
Juan <39202665+ajuancer@users.noreply.github.com>
2020-08-13 00:08:41 +0200

    fix land blog page dark mode logo

--- blog/assets/js/home.js
+++ blog/assets/js/home.js
@@ -3,5 +3,5 @@ var date = new Date();
 console.log((date.getHours() > 21 || date.getHours() < 6));
 if ((date.getHours() >= 21 || date.getHours() < 6)) {
   document.documentElement.setAttribute('data-theme', 'dark');
-  document.getElementById('top-logo').src='/assets/media/icons/logo_v2.svg';
+  document.getElementById('top-logo').src='../assets/media/icons/logo_v2.svg';
 }
