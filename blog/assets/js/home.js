42689050294c1d07b0578474c95dde743ce623e6
Juan <39202665+ajuancer@users.noreply.github.com>
2020-08-13 10:12:25 +0200

    mv blog files to blog repo

--- blog/assets/js/home.js
+++
@@ -1,7 +0,0 @@
-
-var date = new Date();
-console.log((date.getHours() > 21 || date.getHours() < 6));
-if ((date.getHours() >= 21 || date.getHours() < 6)) {
-  document.documentElement.setAttribute('data-theme', 'dark');
-  document.getElementById('top-logo').src='../assets/media/icons/logo_v2.svg';
-}
